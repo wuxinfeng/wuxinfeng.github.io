85c30f6ac31d2c370e38d8480af1dd7679f5891e
Jiller <871182444@qq.com>
2015-09-03 19:25:20 +0800

    rollback

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,19 +1,19 @@
 <?php
 // Check for empty fields
-if(empty($_POST['姓名'])  		||
-   empty($_POST['电子邮件']) 		||
-   empty($_POST['电话']) 		||
-   empty($_POST['留言'])	||
-   !filter_var($_POST['电子邮件'],FILTER_VALIDATE_EMAIL))
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
 	
-$name = $_POST['姓名'];
-$email_address = $_POST['电子邮件'];
-$phone = $_POST['电话'];
-$message = $_POST['留言'];
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
